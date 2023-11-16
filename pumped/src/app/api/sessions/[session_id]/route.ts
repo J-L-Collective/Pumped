54365557efc61af62353fefa9ec0e6906f869f68
Lesley <tomosadalesley@gmail.com>
2023-11-16 15:36:05 -0800

    created session post api

--- pumped/src/app/api/sessions/[session_id]/route.ts
+++ pumped/src/app/api/sessions/[session_id]/route.ts
@@ -1,1 +1,11 @@
-import prisma from "../../../../prisma/index";
+import prisma from "../../../../../prisma";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    // Process a POST request
+  } else {
+    // Handle any other HTTP method
+  }
+}
--- pumped/src/app/api/sessions/route.ts
+++ pumped/src/app/api/sessions/route.ts
@@ -2,29 +2,25 @@ import prisma from "../../../../prisma";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: Request) {
   if (req.method === "POST") {
     // Process a POST request
-    try{
-        const new_session = await prisma.sessions.create({
-            data: { current_question: 0 }
-        });
-    } 
-  } else {
-    // Handle any other HTTP method
+    const res = await req.json();
+    // const { name, date, workouts } =
+    //   typeof res.body == "string" ? JSON.parse(req.body) : req.body;
+    console.log("***", res);
+
+    try {
+      console.log("test");
+      const new_session = await prisma.sessions.create({
+        data: { name: res.name, date: res.date },
+      });
+      //   res.json(new_session);
+      console.log("successfully created workout!");
+      return new Response("created new session", { status: 200 });
+    } catch (error) {
+      console.log("Error creating session: ", error);
+      return new Response("", { status: 500 });
+    }
   }
 }
-
-// export async function POST(req: Request) {
-//     try {
-//       // Create a new room with the calculated room_id nextRoomId
-//       const new_room = await prisma.rooms.create({
-//         data: { current_question: 0 }
-//       });
-//       return NextResponse.json({ room_id: new_room.room_id });
-      
-//     } catch (error) {
-//       console.error(`Error creating room: `, error);
-//       return new NextResponse("", { status: 500});
-//     }
-//   }
