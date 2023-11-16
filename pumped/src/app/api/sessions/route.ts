0ccbd6803546d5d2a058ca5a2fc7195b469faf0f
Lesley <tomosadalesley@gmail.com>
2023-11-16 14:03:09 -0800

    removed node from dependencies

--- pumped/src/app/api/sessions/route.ts
+++ pumped/src/app/api/sessions/route.ts
@@ -5,7 +5,26 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     // Process a POST request
+    try{
+        const new_session = await prisma.sessions.create({
+            data: { current_question: 0 }
+        });
+    } 
   } else {
     // Handle any other HTTP method
   }
 }
+
+// export async function POST(req: Request) {
+//     try {
+//       // Create a new room with the calculated room_id nextRoomId
+//       const new_room = await prisma.rooms.create({
+//         data: { current_question: 0 }
+//       });
+//       return NextResponse.json({ room_id: new_room.room_id });
+      
+//     } catch (error) {
+//       console.error(`Error creating room: `, error);
+//       return new NextResponse("", { status: 500});
+//     }
+//   }
